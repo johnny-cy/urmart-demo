b1dab58c3ee3b7c7cf908b7d8cbf30b2706b831a
unknown <user@SFA021910001.sfcl.com.tw>
2021-02-27 02:14:13 +0800

    debug add more path in urls

--- urmart-demo/web/routing.py
+++ urmart-demo/web/routing.py
@@ -6,4 +6,5 @@ websocket_urlpatterns = [
     re_path(r'ws/urmart/update/$', consumers.UrmartConsumer.as_asgi()),
     re_path(r'wss/web/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
     re_path(r'wss/urmart/update/$', consumers.UrmartConsumer.as_asgi()),
+    re_path(r'urmart/update/$', consumers.UrmartConsumer.as_asgi()),
 ]
